fcd4aff38c94a49b7029550327dbd9f659c9eaa5
Aleksey Belchenkov <u608110@gmail.com>
2019-01-08 13:51:21 +0300

    Checkbox - completed Task

--- src/App.js
+++ src/App.js
@@ -24,11 +24,24 @@ class App extends Component {
     ]
   };
 
+  // Toggle Complete
+  markComplete = (id, e) => {
+     this.setState({
+         todos: this.state.todos.map(todo => {
+             if (todo.id === id) {
+                 todo.completed = !todo.completed
+             }
+             return todo;
+         })
+     });
+  };
+
   render() {
     return (
       <div className="App">
         <Todos
           todos={this.state.todos}
+          markComplete={this.markComplete}
         />
       </div>
     );
